7306a79583abffb33fcafe6d5fa396c829db2280
ivitafra <140718248+ivitafra@users.noreply.github.com>
2023-09-05 01:39:57 +0200

    Add example NSYC class control

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "DataInput/data_input.cc"
+#include "Classes/NelsonSiegelYieldCurve.cpp"
+
 int main()
 {
     // Provide the path to your CSV file here
@@ -24,6 +26,35 @@ int main()
         }
         std::cout << std::endl;
     }
+
+
+
+    /* ----------NelsonSiegel Yield Curve control---------- */
+    
+    // Example parameters for the Nelson-Siegel model
+    double beta0 = 0.03;
+    double beta1 = -0.02;
+    double beta2 = -0.01;
+    double lambda = 2.0;
+
+    // Create a Nelson-Siegel yield curve model
+    NelsonSiegelYieldCurve curve(beta0, beta1, beta2, lambda);
+
+    // Define a range of maturities (in years)
+    std::vector<double> maturities = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    // Calculate yields for the specified maturities
+    std::vector<double> yields = curve.calculateYields(maturities);
+
+    // Print the results
+    std::cout << "Maturity (Years)\tYield (%)" << std::endl;
+    for (int i = 0; i < maturities.size(); ++i) {
+        std::cout << maturities[i] << "\t\t\t" << yields[i] * 100.0 << std::endl;
+    }
+
+    /* ---------------------------------------------------- */
+
+
 
     return 0; // Exit successfully
 }
